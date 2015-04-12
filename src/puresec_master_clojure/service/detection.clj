10a2cb55266cd255ad4bc6907e3dea580a76aa19
Fabian Hoffmann <fabian.hoffmann@otto.de>
2015-04-12 12:27:55 +0200

    restructuring of detection service

--- src/puresec_master_clojure/service/detection.clj
+++ src/puresec_master_clojure/service/detection.clj
@@ -1,10 +1,17 @@
 (ns puresec-master-clojure.service.detection
   (:require [puresec-master-clojure.db.core :as db]))
 
+(defn create-successful-result [id]
+  {:state "SUCCESS" :id id})
 
-(defn register-detection-slave
+(defn get-slave-if-exists [zone_name]
+  (first (db/load-slave-detect {:zone_name zone_name})))
+
+(defn register-detection-slave [zone_name zone_description]
   "registers a alarm detection slave. If a slave with this name is already registered, the existing id is returned"
-  [zone_name zone_description]
-  (if (db/register-slave-detect! {:zone_name zone_name :zone_description zone_description})
-    {:state "SUCCESS" :id (:id (first (db/load-slave-detect {:zone_name zone_name})))}
-    {:state "ERROR"}))
+  (let [existing-slave (get-slave-if-exists zone_name)]
+    (if existing-slave
+      (create-successful-result (:id existing-slave))
+      (if (db/register-slave-detect! {:zone_name zone_name :zone_description zone_description})
+        (create-successful-result (:id (first (db/load-slave-detect {:zone_name zone_name}))))
+        {:state "ERROR"}))))
--- test/puresec_master_clojure/service/detection_test.clj
+++ test/puresec_master_clojure/service/detection_test.clj
@@ -3,14 +3,37 @@
         puresec-master-clojure.service.detection
         puresec-master-clojure.db.core))
 
-(deftest test-detection-slave-registration
-         (testing "that the registration of a detection slave is successful"
-                  (with-redefs [register-slave-detect! (fn [_] true)
-                                load-slave-detect (fn [_] [{:id 7}])]
-                    (is (= {:id 7 :state "SUCCESS"}
-                           (register-detection-slave "some zone" "some zone descr")))))
+(deftest test-register-detection-slave
+   (testing "that the registration is successful if slave already exists"
+      (with-redefs [get-slave-if-exists (fn [_] {:id 7 :zone_name "some zone" :zone_description "some zone descr"})]
+        (is (= {:state "SUCCESS" :id 7}
+               (register-detection-slave "some zone" "some zone descr")))))
 
-         (testing "that the registration of a detection slave fails"
-                  (with-redefs [register-slave-detect! (fn [_] false)]
-                    (is (= {:state "ERROR"}
-                           (register-detection-slave "some zone" "some zone descr"))))))
+   (testing "that the registration is successful if slave doesn't exist"
+      (with-redefs [get-slave-if-exists (fn [_] nil)
+                    register-slave-detect! (fn [_] true)
+                    load-slave-detect (fn [_] [{:id 9}])]
+        (is (= {:state "SUCCESS" :id 9}
+               (register-detection-slave "some zone" "some zone descr")))))
+
+   (testing "that the registration returns an error if it fails"
+     (with-redefs [get-slave-if-exists (fn [_] nil)
+                   register-slave-detect! (fn [_] false)]
+       (is (= {:state "ERROR"}
+              (register-detection-slave "some zone" "some zone descr"))))))
+
+(deftest test-create-successful-result
+  (testing "that a valid successful result is created"
+    (is (= {:state "SUCCESS" :id 5}
+           (create-successful-result 5)))))
+
+(deftest test-get-slave-if-exists
+  (testing "that the first (and only) detection slave is returned"
+    (with-redefs [load-slave-detect (fn [_] [{:id 3 :zone_name "some zone" :zone_description "some zone descr"}])]
+      (is (= {:id 3 :zone_name "some zone" :zone_description "some zone descr"}
+             (get-slave-if-exists "some zone")))))
+
+  (testing "that nil is returned if slave doesn't exist"
+    (with-redefs [load-slave-detect (fn [_] [])]
+      (is (= nil
+             (get-slave-if-exists "some zone"))))))
