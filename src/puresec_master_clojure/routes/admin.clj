ee31a12f19e42c9c88e40e3937f38f8462ad5e19
Fabian Hoffmann <fabian.hoffmann@otto.de>
2015-04-19 16:24:16 +0200

    integrate settings page into sceleton and add first view

--- src/puresec_master_clojure/routes/admin.clj
+++ src/puresec_master_clojure/routes/admin.clj
@@ -2,7 +2,8 @@
   (:require [compojure.core :refer [defroutes GET POST context]]
             [ring.util.response :refer [response status redirect content-type]]
             [puresec-master-clojure.service.settings :as settings]
-            [puresec-master-clojure.utils.response :as response-util]))
+            [puresec-master-clojure.utils.response :as response-util]
+            [puresec-master-clojure.layout :as layout]))
 
 (defn api-map-trigger [request]
   (let [detector-id (:detector_id (:params request))
@@ -13,4 +14,5 @@
 
 (defroutes admin-routes
   (context "/admin" []
+    (GET  "/settings" [] (layout/render "settings.html" {}))
     (POST "/notification/map" request (api-map-trigger request))))
--- test/puresec_master_clojure/routes/admin_test.clj
+++ test/puresec_master_clojure/routes/admin_test.clj
@@ -5,8 +5,14 @@
         puresec-master-clojure.service.settings))
 
 (deftest test-api-notify-alarm
-  (testing "that call to alarm notification api workd"
+  (testing "that call to alarm notification api works"
     (with-redefs [map-trigger (fn [_ _] true)]
       (let [response (app (request :post "/admin/notification/map" {:detector_id 7 :trigger_id 9}))]
         (is (= 200 (:status response)))
           (is (= "application/json; charset=utf-8" (get (:headers response) "Content-Type")))))))
+
+(deftest test-settings-page
+  (testing "that call to settings page works"
+    (with-redefs [map-trigger (fn [_ _] true)]
+      (let [response (app (request :get "/admin/settings"))]
+        (is (= 200 (:status response)))))))
