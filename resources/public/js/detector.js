0394a6a4fdc3d6ee54a686d9f4701ebadaf1702b
Fabian Hoffmann <fabian.hoffmann@otto.de>
2015-04-21 20:38:42 +0200

    more modules in js

--- resources/public/js/detector.js
+++ resources/public/js/detector.js
@@ -1,27 +1,14 @@
 var puresec = puresec || {};
 
-puresec.trigger = function(trigger) {
-    var $trigger = $(trigger);
-    var triggerId = $trigger.data("trigger-id");
-    
-    var toggleState = function() {
-        $trigger
-            .find(".trigger_state")
-            .toggleClass("trigger_state_mapped");
+puresec.detector = function(detector) {
+    var $detector = $(detector);
+    var detectorId = $detector.data("detector-id");
+
+    var getId = function() {
+        return detectorId;
     };
-    
-    var mapDetector = function(detectorId) {
-        $.post("/admin/notification/map", {
-            "detector_id": detectorId,
-            "trigger_id": triggerId
-        }, function(response) {
-            if (response.state == "SUCCESS") {
-                toggleState();
-            }
-        });
-    };
-    
+
     return {
-        "mapDetector": mapDetector
+        "getId": getId
     };
 };
