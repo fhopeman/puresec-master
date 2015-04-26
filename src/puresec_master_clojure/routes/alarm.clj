01522ac0ce2f581a0cf37d0f3d82e6a938b146df
Fabian Hoffmann <fabian.hoffmann@otto.de>
2015-04-26 14:49:04 +0200

    add alerted triggers to response

--- src/puresec_master_clojure/routes/alarm.clj
+++ src/puresec_master_clojure/routes/alarm.clj
@@ -18,9 +18,7 @@
 (defn api-notify-alarm [request]
   (let [detector_id (:detector_id (:params request))]
     (if detector_id
-      (do
-        (dispatcher/dispatch-alarm-notification detector_id)
-        (response (response-utils/create-successful-result)))
+      (response (response-utils/create-successful-result (dispatcher/dispatch-alarm-notification detector_id)))
       (status (response (response-utils/create-error-result "missing parameter detector_id")), 400))))
 
 (defroutes alarm-routes
