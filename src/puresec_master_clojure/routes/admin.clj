18554adf00553195949bb0d4e5cd0930542d5fa7
Fabian Hoffmann <fabian.hoffmann@otto.de>
2015-05-02 21:18:08 +0200

    add api to en/disable the system

--- src/puresec_master_clojure/routes/admin.clj
+++ src/puresec_master_clojure/routes/admin.clj
@@ -3,7 +3,8 @@
             [ring.util.response :refer [response status redirect content-type]]
             [puresec-master-clojure.service.settings :as settings]
             [puresec-master-clojure.utils.response :as response-util]
-            [puresec-master-clojure.layout :as layout]))
+            [puresec-master-clojure.layout :as layout]
+            [puresec-master-clojure.service.notification-dispatcher :as dispatcher]))
 
 (defn api-map-trigger [request]
   (let [detector-id (:detector_id (:params request))
@@ -18,8 +19,14 @@
     (settings/unmap-trigger detector-id trigger-id)
     (response (response-util/create-successful-result))))
 
+(defn api-switch-alarm-state [switch]
+  (switch)
+  (response-util/create-successful-result))
+
 (defroutes admin-routes
   (context "/admin" []
     (GET  "/settings" [] (layout/render "settings.html" {:trigger_mappings (settings/get-trigger-mapping)}))
     (POST "/notification/map" request (api-map-trigger request))
-    (POST "/notification/unmap" request (api-unmap-trigger request))))
+    (POST "/notification/unmap" request (api-unmap-trigger request))
+    (POST "/enable" request (api-switch-alarm-state dispatcher/enable-alarm))
+    (POST "/disable" request (api-switch-alarm-state dispatcher/disable-alarm))))
