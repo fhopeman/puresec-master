569ccb7a0df26a62b62cef9d3909b532b9de55e7
Fabian Hoffmann <fabianhoffmann_@web.de>
2015-05-10 12:18:55 +0200

    refactor dev setup script

--- bin/setupLinux.sh
+++ bin/setupLinux.sh
@@ -2,6 +2,12 @@
 
 set -e
 
+if [ $# -ne 2 ]; then
+    echo "Usage:"
+    echo "setupLinux dbRootUser dbRootPassword"
+    exit 1
+fi
+
 # install leiningen
 echo -e "\e[1minstall latest version of leiningen\e[0m"
 mkdir -p ~/tools
@@ -18,7 +24,10 @@ sudo apt-get install mysql-server
 
 # init database
 echo -e "\e[1mset up database\e[0m"
-echo "CREATE database IF NOT EXISTS puresec_master" | mysql -u$1 -p$2
+echo "DROP database IF EXISTS puresec_master" | mysql -u$1  -p$2
+echo "CREATE database puresec_master" | mysql -u$1 -p$2
+echo "GRANT USAGE ON *.* TO 'psec_master'@'localhost'" | mysql -u$1 -p$2
+echo "DROP USER 'psec_master'@'localhost'" | mysql -u$1 -p$2
 echo "CREATE USER 'psec_master'@'localhost' IDENTIFIED BY 'psec_master';" | mysql -u$1 -p$2
 echo "GRANT ALL PRIVILEGES ON puresec_master.* TO 'psec_master'@'localhost';" | mysql -u$1 -p$2
 lein ragtime migrate
